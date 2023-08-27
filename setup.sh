b43446fa9ee4489f828eb259edeb314f2ccdeb3d
Martin <martinmenchon@gmail.com>
2023-08-27 18:38:44 +0200

    problem with mkdir

--- setup.sh
+++ setup.sh
@@ -1,14 +1,7 @@
-mkdir -p ~/.streamlit/
+#mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
 email = \"martinmenchon@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-enableStaticServing=true\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
